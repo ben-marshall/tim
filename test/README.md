5ebbb0e600e93124718e181884efb7577003daef
Ben Marshall <bm12656@my.bristol.ac.uk>
2015-05-29 12:25:26 +0100

    Added a single asm test to check that the parser works.
     On branch dev/asm
     Changes to be committed:
    	modified:   ../test/README.md
    	new file:   ../test/asm-src/00-parse.s

---
+++ test/README.md
@@ -0,0 +1,6 @@
+
+
+TESTS
+=========================
+
+This folder contains all sources for tests on the different parts of the toolchain and the hardware.
---
+++ test/asm-src/00-parse.s
@@ -0,0 +1,66 @@
+;
+; This test is not designed to produce a useful result. It simply provides every version of an
+; instruction and make sure that the parser works on them as expected.
+;
+
+JUMP main       ; Common setup to jump over the data section.
+
+DATA  0xABCDEF
+DATA  0x0DEAD0
+
+.main 
+    CALL body
+    HALT
+
+.body
+                    ; Register versions of instructions.
+    LOAD  $R0 $R2 $R2
+    STORE $R0 $R2 $R2 
+    PUSH  $R0
+    POP   $R0
+    MOV   $R0 $R2
+    TEST  $R0 $R2
+    AND   $R0 $R2 $R2 
+    NAND  $R0 $R2 $R2 
+    OR    $R0 $R2 $R2 
+    NOR   $R0 $R2 $R2 
+    XOR   $R0 $R2 $R2 
+    LSL   $R0 $R2 $R2 
+    LSR   $R0 $R2 $R2 
+    NOT   $R0 $R2
+    IADD  $R0 $R2 $R2 
+    ISUB  $R0 $R2 $R2 
+    IMUL  $R0 $R2 $R2 
+    IDIV  $R0 $R2 $R2 
+    IASR  $R0 $R2 $R2 
+    FADD  $R0 $R2 $R2 
+    FSUB  $R0 $R2 $R2 
+    FMUL  $R0 $R2 $R2 
+    FDIV  $R0 $R2 $R2 
+    FASR  $R0 $R2 $R2 
+    NOP   
+    SLEEP $R6
+                    ; Immediate versions of instructions.    
+    LOAD   $R1 $R2 0xAB4
+    STORE  $R1 $R2 0xAB4 
+    MOV    $R1 0xAB4 
+    TEST   $R2 0xAB4 
+    AND    $R1 $R2 0xAB4 
+    NAND   $R1 $R2 0xAB4 
+    OR     $R1 $R2 0xAB4 
+    NOR    $R1 $R2 0xAB4 
+    XOR    $R1 $R2 0xAB4 
+    LSL    $R1 $R2 0xAB4 
+    LSR    $R1 $R2 0xAB4 
+    IADD   $R1 $R2 0xAB4 
+    ISUB   $R1 $R2 0xAB4 
+    IMUL   $R1 $R2 0xAB4 
+    IDIV   $R1 $R2 0xAB4 
+    IASR   $R1 $R2 0xAB4 
+    FADD   $R1 $R2 0xAB4 
+    FSUB   $R1 $R2 0xAB4 
+    FMUL   $R1 $R2 0xAB4 
+    FDIV   $R1 $R2 0xAB4 
+    FASR   $R1 $R2 0xAB4 
+    SLEEP  0xAB4 
+    RETURN
