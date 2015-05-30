531190ea7a66f56bdfe743e63f56763235f3511a
Ben Marshall <bm12656@my.bristol.ac.uk>
2015-05-30 13:15:15 +0100

    Implemented parsing of all instructions with three operands.  On branch dev/asm-rewrite  Changes to be committed: 	modified:   ../sw/asm/asm_parse.c

--- sw/asm/asm_parse.c
+++ sw/asm/asm_parse.c
@@ -84,6 +84,165 @@ asm_lex_token * asm_parse_call_jump(asm_statement * statement, asm_lex_token * t
     return operand -> next;
 }
 
+
+/*!
+@brief Responsible for parsing instructions with three register operands.
+@param [inout] statement - Resulting statment to set members of.
+@param [in] token - The token which to parse into a statement. Several subsequent tokens may also
+be eaten.
+@param errors - Error counter pointer.
+@returns The next token that should be parsed, i.e. the one following the last token eaten by this
+function.
+*/
+asm_lex_token * asm_parse_three_register_operands(asm_statement * statement, asm_lex_token * token, int * errors)
+{
+    asm_lex_token * opcode    = token;
+    asm_lex_token * operand_1 = opcode    -> next;
+    asm_lex_token * operand_2 = operand_1 -> next;
+    asm_lex_token * operand_3 = operand_2 -> next;
+    
+    assert(opcode -> type == OPCODE);
+    assert(operand_1 -> type == REGISTER);
+    assert(operand_2 -> type == REGISTER);
+    assert(operand_3 -> type == REGISTER);
+
+    statement -> args.reg_reg_reg.reg_1 = operand_1 -> value.reg;
+    statement -> args.reg_reg_reg.reg_2 = operand_2 -> value.reg;
+    statement -> args.reg_reg_reg.reg_3 = operand_3 -> value.reg;
+
+    statement -> size = 3;
+    
+    switch(opcode -> value.opcode)
+    {
+        case(LEX_LOAD):  statement -> opcode = LOADR; break;
+        case(LEX_STORE): statement -> opcode = STORR; break;
+        case(LEX_AND ):  statement -> opcode = ANDR ; break;
+        case(LEX_NAND):  statement -> opcode = NANDR; break;
+        case(LEX_OR  ):  statement -> opcode = ORR  ; break;
+        case(LEX_NOR ):  statement -> opcode = NORR ; break;
+        case(LEX_XOR ):  statement -> opcode = XORR ; break;
+        case(LEX_LSL ):  statement -> opcode = LSLR ; break;
+        case(LEX_LSR ):  statement -> opcode = LSRR ; break;
+        case(LEX_IADD):  statement -> opcode = IADDR; break;
+        case(LEX_ISUB):  statement -> opcode = ISUBR; break;
+        case(LEX_IMUL):  statement -> opcode = IMULR; break;
+        case(LEX_IDIV):  statement -> opcode = IDIVR; break;
+        case(LEX_IASR):  statement -> opcode = IASRR; break;
+        case(LEX_FADD):  statement -> opcode = FADDR; break;
+        case(LEX_FSUB):  statement -> opcode = FSUBR; break;
+        case(LEX_FMUL):  statement -> opcode = FMULR; break;
+        case(LEX_FDIV):  statement -> opcode = FDIVR; break;
+        case(LEX_FASR):  statement -> opcode = FASRR; break;
+
+        case(LEX_ERROR):
+            error("Bad Token!\n");
+            return operand_3 -> next;
+
+        default:
+            error("This function doesnt support parsing of asm opcode %d \n", opcode -> value.opcode);
+            return operand_3 -> next;
+
+    }
+
+    return operand_3 -> next;
+}
+
+/*!
+@brief Responsible for parsing instructions with two register operands and one immediate operand.
+@param [inout] statement - Resulting statment to set members of.
+@param [in] token - The token which to parse into a statement. Several subsequent tokens may also
+be eaten.
+@param errors - Error counter pointer.
+@returns The next token that should be parsed, i.e. the one following the last token eaten by this
+function.
+*/
+asm_lex_token * asm_parse_two_register_one_immediate(asm_statement * statement, asm_lex_token * token, int * errors)
+{
+    asm_lex_token * opcode    = token;
+    asm_lex_token * operand_1 = opcode    -> next;
+    asm_lex_token * operand_2 = operand_1 -> next;
+    asm_lex_token * operand_3 = operand_2 -> next;
+    
+    assert(opcode -> type == OPCODE);
+    assert(operand_1 -> type == REGISTER);
+    assert(operand_2 -> type == REGISTER);
+    assert(operand_3 -> type == IMMEDIATE);
+
+    statement -> args.reg_reg_immediate.reg_1 = operand_1 -> value.reg;
+    statement -> args.reg_reg_immediate.reg_2 = operand_2 -> value.reg;
+    statement -> args.reg_reg_immediate.immidiate = operand_3 -> value.immediate;
+
+    statement -> size = 3;
+    
+    switch(opcode -> value.opcode)
+    {
+        case(LEX_LOAD):  statement -> opcode = LOADI; break;
+        case(LEX_STORE): statement -> opcode = STORI; break;
+        case(LEX_AND ):  statement -> opcode = ANDI ; break;
+        case(LEX_NAND):  statement -> opcode = NANDI; break;
+        case(LEX_OR  ):  statement -> opcode = ORI  ; break;
+        case(LEX_NOR ):  statement -> opcode = NORI ; break;
+        case(LEX_XOR ):  statement -> opcode = XORI ; break;
+        case(LEX_LSL ):  statement -> opcode = LSLI ; break;
+        case(LEX_LSR ):  statement -> opcode = LSRI ; break;
+        case(LEX_IADD):  statement -> opcode = IADDI; break;
+        case(LEX_ISUB):  statement -> opcode = ISUBI; break;
+        case(LEX_IMUL):  statement -> opcode = IMULI; break;
+        case(LEX_IDIV):  statement -> opcode = IDIVI; break;
+        case(LEX_IASR):  statement -> opcode = IASRI; break;
+        case(LEX_FADD):  statement -> opcode = FADDI; break;
+        case(LEX_FSUB):  statement -> opcode = FSUBI; break;
+        case(LEX_FMUL):  statement -> opcode = FMULI; break;
+        case(LEX_FDIV):  statement -> opcode = FDIVI; break;
+        case(LEX_FASR):  statement -> opcode = FASRI; break;
+
+        case(LEX_ERROR):
+            error("Bad Token!\n");
+            return operand_3 -> next;
+
+        default:
+            error("This function doesnt support parsing of asm opcode %d \n", opcode -> value.opcode);
+            return operand_3 -> next;
+
+    }
+
+    return operand_3 -> next;
+}
+
+
+/*!
+@brief Responsible for parsing instructions with three operands..
+@param [inout] statement - Resulting statment to set members of.
+@param [in] token - The token which to parse into a statement. Several subsequent tokens may also
+be eaten.
+@param errors - Error counter pointer.
+@returns The next token that should be parsed, i.e. the one following the last token eaten by this
+function.
+*/
+asm_lex_token * asm_parse_three_operand(asm_statement * statement, asm_lex_token * token, int * errors)
+{
+    asm_lex_token * opcode    = token;
+    asm_lex_token * operand_1 = opcode    -> next;
+    asm_lex_token * operand_2 = operand_1 -> next;
+    asm_lex_token * operand_3 = operand_2 -> next;
+
+    if(operand_3->type == IMMEDIATE)
+    {
+        return asm_parse_two_register_one_immediate(statement, opcode, errors);
+    }
+    else if(operand_3->type == REGISTER)
+    {
+        return asm_parse_three_register_operands(statement, opcode, errors);
+    }
+    else
+    {
+        error("Line %d: Expected immediate or register, but got token type %d\n", operand_3->line_number, operand_3->type);
+        return operand_3 -> next;
+    }
+
+}
+
+
 /*!
 @brief Responsible for selecting which function should parse the next few tokens.:w
 @param token - The token containing the  label value
@@ -100,6 +259,27 @@ asm_lex_token * asm_parse_opcode(asm_statement * statement, asm_lex_token * toke
         case(LEX_JUMP):
         case(LEX_CALL):
             return asm_parse_call_jump(statement, token, errors);
+        
+        case(LEX_LOAD):
+        case(LEX_STORE):
+        case(LEX_AND ): 
+        case(LEX_NAND): 
+        case(LEX_OR  ): 
+        case(LEX_NOR ): 
+        case(LEX_XOR ): 
+        case(LEX_LSL ): 
+        case(LEX_LSR ): 
+        case(LEX_IADD): 
+        case(LEX_ISUB): 
+        case(LEX_IMUL): 
+        case(LEX_IDIV): 
+        case(LEX_IASR): 
+        case(LEX_FADD): 
+        case(LEX_FSUB): 
+        case(LEX_FMUL): 
+        case(LEX_FDIV): 
+        case(LEX_FASR): 
+            return asm_parse_three_operand(statement, token,errors);
 
         case(LEX_ERROR):
             error("Bad Token!\n");
