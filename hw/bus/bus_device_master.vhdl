cff542031fbfaecf83263dc8a4554859e75d646c
Ben Marshall <bm12656@my.bristol.ac.uk>
2015-06-08 15:10:04 +0100

    Added todo notes.

--- hw/bus/bus_device_master.vhdl
+++ hw/bus/bus_device_master.vhdl
@@ -3,6 +3,7 @@
 --! @file  bus_device_master.vhdl
 --! @brief Contains the architecture declaration of the master bus device. This architecture belongs
 --!        to the bus_device entity.
+--! @todo  Write this architecture definition.
 --!
 --! ------------------------------------------------------------------------------------------------
 
--- hw/bus/bus_device_slave.vhdl
+++ hw/bus/bus_device_slave.vhdl
@@ -3,6 +3,7 @@
 --! @file  bus_device_slave.vhdl
 --! @brief Contains the architecture declaration of the slave bus device. This architecture belongs
 --!        to the bus_device entity.
+--! @todo  Write this architecture definition.
 --!
 --! ------------------------------------------------------------------------------------------------
 
