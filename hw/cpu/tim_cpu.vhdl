2ce6ee6a01336403bfc904fc116f9d0ae3b2137e
Ben Marshall <bm12656@my.bristol.ac.uk>
2015-06-08 15:35:58 +0100

    Cleaned up the CPU hardware RTL folder with new entity declaration ready for re-implementation.
     On branch master-cleanup
     Changes to be committed:
    	deleted:    ../hw/cpu/tb_tim_cpu.vhdl
    	modified:   ../hw/cpu/tim_cpu.vhdl
    	new file:   ../hw/cpu/tim_cpu_arch.vhdl
    	deleted:    ../hw/cpu/tim_cpu_arch_basic.vhdl
    	deleted:    ../hw/cpu/tim_cpu_fetch.vhdl
    	deleted:    ../hw/cpu/tim_cpu_fetch_arch.vhdl
    
     Untracked files:
    	../hw/bus/bus_device_testbench.vhdl

--- hw/cpu/tim_cpu.vhdl
+++ hw/cpu/tim_cpu.vhdl
@@ -1,17 +1,18 @@
 --! ------------------------------------------------------------------------------------------------
 --!
 --! @file tim_cpu.vhdl
---! @brief Top file for the CPU module entity.
+--! @brief Top entity declaration file for the CPU module entity.
 --!
 --! ------------------------------------------------------------------------------------------------
 
 
+--! Use the standard IEEE libraries
 library ieee;
+--! Import standard logic interfaces.
 use ieee.std_logic_1164.ALL;
+--! Standard numeric operations and types.
 use ieee.numeric_std.ALL;
 
---! Imported from tim_common package,
-use work.tim_common.word_width;
 
 --! The top module of the tim CPU core.
 entity tim_cpu is
@@ -22,24 +23,24 @@ entity tim_cpu is
         reset                   : in    std_logic;
         --! Set high IFF the CPU has halted processing.
         halted                  : out   std_logic;
-        
-        --! The lines which carry data and addresses;
-        memory_bus_lines        : inout std_logic_vector(word_width-1 downto 0);
-        --! Used to assert data written by the bus master to bus_lines is valid.
-        memory_bus_valid        : out   std_logic;
-        --! Used to assert that the slave has read the bus lines and they can be updated.
-        memory_bus_enable       : in    std_logic;
-        --! High if this transaction is a write, low if it is a read.
-        memory_bus_read_write   : out   std_logic;
 
-        --! The lines which carry data and addresses;
-        device_bus_lines        : inout std_logic_vector(word_width-1 downto 0);
-        --! Used to assert data written by the bus master to bus_lines is valid.
-        device_bus_valid        : out   std_logic;
-        --! Used to assert that the slave has read the bus lines and they can be updated.
-        device_bus_enable       : in    std_logic;
-        --! High if this transaction is a write, low if it is a read.
-        device_bus_read_write   : out   std_logic
+        --
+        -- Memory and data bus interface. These will be connected to a master bus controller
+        -- architecture inside the cpu core.
+        --
+        
+        --! The current address of the thing being accessed on the bus.
+        mem_bus_address_lines   : inout std_logic_vector(address_width-1 downto 0);
+        --! The data being carried on the bus.
+        mem_bus_data_lines      : inout std_logic_vector(data_width-1 downto 0);
+        --! Signal to tell the rest of the bus that the address lines are valid, initiating a transaction.
+        mem_bus_address_valid   : inout std_logic;
+        --! Signal to tell the rest of the bus that the data lines are valid.
+        mem_bus_data_valid      : inout std_logic;
+        --! Enable signal to let slaves tell the master they are finished with the data on the bus.
+        mem_bus_enable          : inout std_logic;
+        --! high = write, low = read operation.
+        mem_bus_write_enable    : inout std_logic;
     );
 end entity tim_cpu;
 
---
+++ hw/cpu/tim_cpu_arch.vhdl
@@ -0,0 +1,14 @@
+--! ------------------------------------------------------------------------------------------------
+--!
+--! @file tim_cpu_arch.vhdl
+--! @brief Contains the architecture declaration/defintion for the TIM CPU module.
+--!
+--! ------------------------------------------------------------------------------------------------
+
+
+--! Use the standard IEEE libraries
+library ieee;
+--! Import standard logic interfaces.
+use ieee.std_logic_1164.ALL;
+--! Standard numeric operations and types.
+use ieee.numeric_std.ALL;
