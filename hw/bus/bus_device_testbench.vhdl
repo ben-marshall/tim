a1b1be26b146c872b4ef359f21225c4a61598e70
Ben Marshall <bm12656@my.bristol.ac.uk>
2015-06-08 15:36:28 +0100

    Added an empty bus device testbench file.  On branch master-cleanup  Changes to be committed: 	new file:   ../hw/bus/bus_device_testbench.vhdl

---
+++ hw/bus/bus_device_testbench.vhdl
@@ -0,0 +1,14 @@
+--! ------------------------------------------------------------------------------------------------
+--!
+--! @file  bus_device_testbench.vhdl
+--! @brief Contains the testbench for all bus devices.
+--!
+--! ------------------------------------------------------------------------------------------------
+
+--! Use the standard IEEE libraries
+library ieee;
+--! Import standard logic interfaces.
+use ieee.std_logic_1164.ALL;
+--! Standard numeric operations and types.
+use ieee.numeric_std.ALL;
+
