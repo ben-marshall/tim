c144bba67e1175cdac50da97e6c84ab430f3ae1c
Ben Marshall <bm12656@my.bristol.ac.uk>
2015-06-14 09:04:06 +0100

    Added internal decoded signals to the cpu architecture.
     On branch dev-hw-cpu
     Changes to be committed:
    	modified:   cpu/tim_cpu_arch.vhdl

--- hw/cpu/tim_cpu_arch.vhdl
+++ hw/cpu/tim_cpu_arch.vhdl
@@ -15,33 +15,37 @@ use ieee.numeric_std.ALL;
 
 --! Architecture declaration for the cpu core.
 architecture rtl of tim_cpu is
-
-    --! A bram memory module connected to the system bus.
-    component mem_bus_bram    is
-        generic(
-            address_bottom  : unsigned  := to_unsigned(0, memory_word_width);
-            address_top     : unsigned  := to_unsigned(0, memory_word_width)
-        );
-        port(
-            clk                 : in    std_logic;
-            reset               : in    std_logic;
-            bus_address_lines   : inout unsigned(31 downto 0);
-            bus_data_lines      : inout std_logic_vector(31 downto 0);
-            bus_address_valid   : inout std_logic;
-            bus_data_valid      : inout std_logic;
-            bus_enable          : inout std_logic;
-            bus_write_enable    : inout std_logic
-        );
-    end component mem_bus_bram;
         
-    signal req_bus_address_lines    : unsigned(address_bus_width-1 downto 0);
-    signal req_bus_data_lines       : std_logic_vector(data_bus_width-1 downto 0);
-    signal req_bus_pending          : std_logic;
-    signal req_bus_complete         : std_logic;
-    signal req_bus_write_enable     : std_logic;
+    --! Address lines for connecting to the master bus controller.
+    signal req_bus_address_lines        : unsigned(address_bus_width-1 downto 0);
+    --! Data lines for connecting to the master bus controller.
+    signal req_bus_data_lines           : std_logic_vector(data_bus_width-1 downto 0);
+    --! Request pending lines for connecting to the master bus controller.
+    signal req_bus_pending              : std_logic;
+    --! Request complete lines for connecting to the master bus controller.
+    signal req_bus_complete             : std_logic;
+    --! Write enable lines for connecting to the master bus controller.
+    signal req_bus_write_enable         : std_logic;
+    
+    --! Global program counter, temporary until the register file module is finished.
+    signal program_counter              : unsigned(address_bus_width-1 downto 0) := (others => '0');
 
+    --! is the currently decoded instruction valid?
+    signal decoded_instruction_valid    : std_logic;
+    --! The size in bytes of the decoded instruction,
+    signal decoded_instruction_size     : integer := 0;
     --! The most recently fetched and decoded instruction.
-    signal decoded_instruction      : tim_instruction;
+    signal decoded_instruction          : tim_instruction;
+    --! The condition code of the decoded instruction.
+    signal decoded_condition            : tim_instruction_condition;
+    --! The immediate of the decoded instruction, aligned to the LSB.
+    signal decoded_immediate            : std_logic_vector(immediate_width-1 downto 0);
+    --! Decoded Register One
+    signal decoded_reg_1                : tim_register;
+    --! Decoded Register Two 
+    signal decoded_reg_2                : tim_register;
+    --! Decoded Register Three
+    signal decoded_reg_3                : tim_register;
 
 begin
     
@@ -55,18 +59,24 @@ begin
         end if;
     end process program_counter_update;
 
-    --! The fetch decode module.
+    --! The fetch decode module. Currently this is just hooked directly into the master bus controller
+    --! eventually it will attatch via a controller multiplexer so other modules can access the bus.
     fetch_module    : entity work.tim_cpu_fetch_decode
     port map(
         clk                   => clk, 
         reset                 => reset,
         program_counter       => program_counter,
         
-        instruction_recieved  => '1',
-        instruction_valid     => instruction_valid,
+        instruction_recieved  => '1', -- tied high for now to constantly fetch instructions.
+        instruction_valid     => decoded_instruction_valid,
         
         decoded_instruction_size => decoded_instruction_size,
         decoded_instruction      => decoded_instruction,
+        decoded_condition        => decoded_condition,
+        decoded_immediate        => decoded_immediate,
+        decoded_reg_1            => decoded_reg_1,
+        decoded_reg_2            => decoded_reg_2,
+        decoded_reg_3            => decoded_reg_3,
 
         req_address_lines     => req_bus_address_lines,
         req_data_lines        => req_bus_data_lines,
@@ -103,4 +113,4 @@ begin
     );
 
 
-end architecture testbench;
+end architecture rtl;
